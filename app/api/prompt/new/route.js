81955324b9504e9d6013580096fa05b14a150d53
Vinayak <vinayakcpa@gmail.com>
2023-06-22 11:16:55 -0400

    implementing posting of prompts feature

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,9 +1,23 @@
 import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
 
-export const POST = async (req, res) => {
+export const POST = async (req) => {
   const { userID, prompt, tag } = await req.json();
 
   try {
     await connectToDB();
-  } catch (error) {}
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+
+    return newResponse(JSON.stringify(newPrompt), {
+      status: 201,
+    });
+  } catch (error) {
+    return new Response("Failed to create a new prompt", { status: 500 });
+  }
 };
