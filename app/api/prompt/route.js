dccbed83d7e202b55452c03860762a57d8a95492
Vinayak <vinayakcpa@gmail.com>
2023-06-22 12:03:49 -0400

    added feed feature with copy to clipboard functionality

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -10,5 +10,9 @@ export const GET = async (request) => {
     return new Response(JSON.stringify(prompts), {
       status: 200,
     });
-  } catch (error) {}
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", {
+      status: 500,
+    });
+  }
 };
