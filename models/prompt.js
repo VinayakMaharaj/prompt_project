9f7a5b54329af419d4adcb6982b8e1d8ed3b0965
Vinayak <vinayakcpa@gmail.com>
2023-06-22 13:12:55 -0400

    fixed prompt model

--- models/prompt.js
+++ models/prompt.js
@@ -1,13 +1,17 @@
-import { Schema, model, models, mongo } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 const PromptSchema = new Schema({
   creator: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: "User",
   },
   prompt: {
     type: String,
     required: [true, "Prompt is required."],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required."],
   },
 });
 
